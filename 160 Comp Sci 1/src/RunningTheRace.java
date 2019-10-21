05de754a47d6cc11c0eb98b2d516d0482cfa7db6
Shallom196 <39003065+Shallom196@users.noreply.github.com>
2019-10-20 23:00:38 -0500

    3 Commit, Fixed, code now works!

--- 160 Comp Sci 1/src/RunningTheRace.java
+++ 160 Comp Sci 1/src/RunningTheRace.java
@@ -2,34 +2,65 @@ import java.util.Scanner;
 public class RunningTheRace {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String runner1, runner2, runner3;
-		Scanner keyboard = new Scanner (System.in);
-		System.out.print("What are the runners names ?\n");
+		double runner1Time, runner2Time, runner3Time;
+		Scanner keyboard = new Scanner (System.in); //Declared a scanner class
+
+
+		System.out.print("What are the first runner's names ?\n");
 		runner1 = keyboard.nextLine();
+		System.out.print("How long in mintues did it take " + runner1 +" to complete his run?\n");
+		runner1Time = keyboard.nextDouble();
+		keyboard.nextLine();		//takes line break away.
+		System.out.print("What are the second runner's names ?\n");
 		runner2 = keyboard.nextLine();
+
+		System.out.print("How long in mintues did it take " + runner2 +" to complete his run?\n");
+		runner2Time = keyboard.nextDouble();
+		keyboard.nextLine();		//takes line break away.
+		System.out.print("What are the third runner's names ?\n");
 		runner3 = keyboard.nextLine();
-		
-		int runner1Time, runner2Time, runner3Time;
-		System.out.print("How long in mintues did it take them to" +
-		"complete the race respectivelty ?\n");
-		runner1Time = keyboard.nextInt();
-		runner2Time = keyboard.nextInt();
-		runner3Time = keyboard.nextInt();
-		
-		if (runner1Time >= runner2Time && runner2Time >= runner3Time )
-		    System.out.print("Here are the positions: " + runner1 + "\n"
-		    		+ runner2 +"\n"
-		    		+runner3);
-	    else if (runner2Time >= runner1Time && runner1Time >= runner3Time)
-					 System.out.print("Here are the positions: " + runner2 + "\n"
-					    		+ runner1 +"\n"
-					    		+runner3);
-	    else if (runner3Time >= runner1Time && runner1Time >= runner2Time)
-			 System.out.print("Here are the positions: " + runner3 + "\n"
-			    		+ runner2 +"\n"
-			    		+runner1);
-		
-	}
+		System.out.print("How long in mintues did it take " + runner3 +" to complete his run?\n");
+		runner3Time = keyboard.nextDouble();
+
+		if (runner1Time < runner2Time && runner1Time < runner3Time ){
+		    System.out.print( " " +runner1 + " finished 1st position, with " + runner1Time+ " minutes\n");
+			if (runner2Time < runner3Time ){
+				System.out.print(" "+runner2 + " finshed 2nd position, with " + runner2Time + " minutes\n"
+								 +" "+runner3 + " finshed 3rd position, with " + runner3Time + " minutes\n");
+						 }
+			 else{
+				    System.out.print(" "+runner3 + " finshed 2nd position, with " + runner3Time + " minutes\n"
+				    +" "+runner2 + " finshed 3rd position, with " + runner2Time + "minutes\n");
+}
+}
+	else if (runner2Time < runner1Time && runner2Time < runner3Time ){
+				    System.out.print( " " +runner2 + " finished 1st position, with " + runner2Time+ " minutes\n");
+					if (runner1Time < runner3Time ){
+						System.out.print(" "+runner1 + "finshed 2nd position, with " + runner1Time + " minutes\n"
+										 +" "+runner3 + " finshed 3rd position, with " + runner3Time + " minutes\n");
+						}
+
+					else{
+						    System.out.print(" "+runner3 + " finshed 2nd position, with " + runner3Time + " minutes\n"
+						    +" "+runner1 + " finshed 3rd position, with " + runner1Time + "\n");
+}
+}
+
+		else if (runner3Time < runner1Time && runner3Time < runner2Time ){
+						    System.out.print( " " +runner3 + " finished 1st position, with " + runner3Time+ " minutes\n");
+							if (runner2Time < runner1Time ){
+								System.out.print(" "+runner2 + "finshed 2nd position, with " + runner2Time + " minutes\n"
+												 +" "+runner1 + " finshed 3rd position, with " + runner1Time + " minutes\n");
+									}
+							else {
+								    System.out.print(" "+runner1 + " finshed 2nd position, with " + runner1Time + " minutes\n"
+								    +" "+runner2 + " finshed 3rd position, with " + runner2Time + " minutes\n");
 
 }
+}
+
+
+
+					 }
+	}
